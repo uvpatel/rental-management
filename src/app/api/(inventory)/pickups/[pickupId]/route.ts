e7c8c4df6eaa1999c18e65c4b3a67d06d31e42fa
Urvil Patel <uvpatel7271@gmail.com>
2026-08-23 22:37:35 +0530

    fix

---
+++ src/app/api/(inventory)/pickups/[pickupId]/route.ts
@@ -0,0 +1,14 @@
+export async function GET() {
+}
+
+export async function POST() {
+}
+
+export async function PUT() {
+}
+
+export async function DELETE() {
+}
+
+export async function PATCH() {
+}
