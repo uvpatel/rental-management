03115c060ccce9bc84fc25494b9a6e228b060224
Urvil Patel <uvpatel7271@gmail.com>
2026-08-23 19:18:38 +0530

    (Dashboard) frontend pages defined

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -20,12 +20,9 @@ import data from "./data.json"
 
 export default function DashboardPage() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+<>
+
+      
             <Separator
               orientation="vertical"
               className="mr-2 data-vertical:h-4 data-vertical:self-auto"
@@ -43,8 +40,7 @@ export default function DashboardPage() {
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
-          </div>
-        </header>
+         
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
@@ -56,7 +52,6 @@ export default function DashboardPage() {
             </div>
           </div>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+  </>
   )
 }
